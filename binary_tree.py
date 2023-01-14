68d3fbf85ca690adbbaed079d67eb2b614618b5e
unknown <Christiandevega12@gmail.com>
2023-01-14 11:46:19 +0800

    added print function

--- binary_tree.py
+++ binary_tree.py
@@ -39,9 +39,9 @@ def build_tree(elements):
     for i in range(1,len(elements)):
         root.add_child(elements[i])
 
-    return
+    return root
 
 if __name__ ==  '__main__':
     numbers = [17,4,1,20,9,23,18,34]
     numbers_tree = build_tree(numbers)
-            
+    print(numbers_tree.in_order_traversal())
