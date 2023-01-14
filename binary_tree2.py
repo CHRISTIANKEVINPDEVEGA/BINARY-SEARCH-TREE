98839baae9c23194b74234c084325c0ac33136b3
unknown <Christiandevega12@gmail.com>
2023-01-14 14:00:05 +0800

    remove the other two traversal method

--- binary_tree2.py
+++ binary_tree2.py
@@ -33,30 +33,6 @@ class BinarySearchNode:
 
         return elements
 
-    def pre_order_traversal(self):
-        elements = []
-
-        elements.append(self.data)
-
-        if self.left:
-            elements += self.left.pre_order_traversal()
-        if self.right:
-            elements += self.right.pre_order_traversal()    
-
-        return elements
-
-    def post_order_traversal(self):
-        elements=[]
-
-        if self.left:
-            elements += self.left.post_order_traversal()
-        if self.right:
-            elements += self.right.post_order_traversal()
-
-        elements.append(self.data)
-
-        return elements
-
     def search(self,val):
         if self.data == val:
             return True
